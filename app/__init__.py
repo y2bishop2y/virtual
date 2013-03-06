a2032bfed7afe4d782fae31ac4430ef81766f9ff
eberenbaum <eberenbaum@okta.com>
2013-03-06 00:15:42 -0800

    timestamp Using Moment JS library to deal with timestamps

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,12 @@ from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from momentjs import momentjs
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals['momentjs'] = momentjs
 
 db = SQLAlchemy(app)
 
